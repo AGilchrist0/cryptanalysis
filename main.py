16f1f6937b17891ae3945a9530aa2d1057829c74
AGilchrist0 <aaron.gilchrist@outlook.com>
2016-10-06 19:16:31 +0100

    Setting up Wordnik

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import FrequencyAnalysis
+from wordnik import *
+apiUrl = 'http://api.wordnik.com/v4'
+apiKey = 'YOUR API KEY HERE'
+client = swagger.ApiClient(apiKey, apiUrl)
 
-cyphertext = input('Please enter the cyphertext.\n--> ')
+cyphertext = input('Please enter the cyphertext.\n--> ').lower()
 print(frequency_analyser.analyse_all_characters(cyphertext))
+
+
+input('')
